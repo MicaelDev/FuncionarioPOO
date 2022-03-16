247c2ec85df145b029e401b50da0c1e7b376f816
Micael20 <74980510+MicaelDev@users.noreply.github.com>
2022-03-15 22:23:27 -0300

    Add project files.

---
+++ FuncionarioPOO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace FuncionarioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario primeiroFuncionario = new Funcionario();
+            Funcionario segundoFuncionario = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            primeiroFuncionario.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            primeiroFuncionario.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            segundoFuncionario.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            segundoFuncionario.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salarioMedio = (primeiroFuncionario.Salario + segundoFuncionario.Salario) / 2.0;
+
+            Console.WriteLine("Salário médio: " + salarioMedio.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
